d951ea4cbb1558e0af8c9ba766c860d4d73852f6
w-red <yterashi@gmail.com>
2015-08-25 19:57:53 +0900

    Initial Commit

---
+++ testRep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testRep
+//
+//  Created by 赫 洋兵 on 2015/08/25.
+//  Copyright (c) 2015年 TERASHI, yohei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
